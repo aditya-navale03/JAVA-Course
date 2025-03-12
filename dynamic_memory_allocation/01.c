f8bcc8ed30ded4128601fa51440ef4796826aca7
Aditya-Navale-03 <aditya.navale.399@gmail.com>
2025-03-12 18:24:22 +0530

    updated in dynamic memory allocation first program

--- dynamic_memory_allocation/01.c
+++ dynamic_memory_allocation/01.c
@@ -2,8 +2,17 @@
 #include<stdlib.h>
 int main() {
 
-    
+int n;
+printf("enter how much elements you want:");
+scanf("%d",&n);
 
+int *ptr=(int *)malloc(n * sizeof(int));
+
+if(ptr != NULL) {
+    printf("memory allocated:[%d]",ptr);
+} else {
+    printf("memory is not allocated");
+}
 
     return 0;
 }
